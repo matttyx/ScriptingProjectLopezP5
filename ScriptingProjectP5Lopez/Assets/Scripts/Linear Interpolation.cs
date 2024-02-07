463cab359d905f6df1996d482149a4bf46a800a2
Matthew Lopez <719710@student.dusd.net>
2024-02-07 13:35:33 -0800

    Linear Interpolations
    made script on class computer, will continue on thinkpad

---
+++ ScriptingProjectP5Lopez/Assets/Scripts/Linear Interpolation.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{ /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This line of code makes it so that
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f * Time.deltaTime);
+    }
+    */
+}
